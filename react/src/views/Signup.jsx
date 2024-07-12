9d8b7782c2fb5274309edbb4587adfbc52e09057
Anthony Premo <ajaipremo@gmail.com>
2024-07-12 12:25:38 -0400

    started backend routes

--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -1,20 +1,55 @@
 import {Link} from "react-router-dom";
-
-const onSubmit = (ev) => {
-    ev.preventDefault()
-}
+import {useRef, useState} from "react";
+import axiosClient from "../axios-client.js";
+import {useStateContext} from "../contexts/ContextProvider.jsx";
 
 export default function Signup() {
+    const nameRef = useRef()
+    const emailRef = useRef()
+    const passwordRef = useRef()
+    const passwordConfirmationRef = useRef()
+    const {errors, setErrors} = useState(null)
+    const {setUser, setToken} = useStateContext()
+
+    const onSubmit = (ev) => {
+        ev.preventDefault()
+        const payload = {
+            name: nameRef.current.value,
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+            password_confirmation: passwordConfirmationRef.current.value,
+        }
+        axiosClient.post('/signup', payload)
+            .then(({data}) => {
+                setUser(data.user)
+                setToken(data.token)
+        })
+            .catch(err => {
+                const response = err.response
+                if (response && response.status === 422) {
+                    setErrors(response.data.errors)
+                }
+            })
+    }
+
     return (
         <div className="login-signup-form animated fadeInDown">
             <div className="form">
                 <form onSubmit={onSubmit}>
-                    <h1 className="title">Create an Account</h1>
-                    <input type="email" placeholder="Full Name"/>
-                    <input type="email" placeholder="Email"/>
-                    <input type="password" placeholder="Password"/>
-                    <input type="password" placeholder="Password Confirmation"/>
-                    <button className="btn btn-block">Login</button>
+                    <h1 className="title">
+                        Create an Account
+                    </h1>
+                    {errors && <div className="alert">
+                        {Object.keys(errors).map(key => (
+                            <p key={key}>{errors[key][0]}</p>
+                        ))}
+                    </div>
+                    }
+                    <input ref={nameRef} placeholder="Full Name"/>
+                    <input ref={emailRef} type="email" placeholder="Email"/>
+                    <input ref={passwordRef} type="password" placeholder="Password"/>
+                    <input ref={passwordConfirmationRef} type="password" placeholder="Password Confirmation"/>
+                    <button className="btn btn-block">Create Account</button>
                     <p className="message">
                         Already have an account? <Link to="/login">Login</Link>
                     </p>
